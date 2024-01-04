382aae0e427b8036386c5504227b1305e19cfc3e
Ketan <ketanm161@gmail.com>
2024-01-04 23:09:53 +0530

    Add react-router-dom package and implement routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
-	return <h1 className="text-red-500">Hello World</h1>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/sign-in" element={<SignIn />} />
+				<Route path="/sing-up" element={<SignUp />} />
+				<Route path="/about" element={<About />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
