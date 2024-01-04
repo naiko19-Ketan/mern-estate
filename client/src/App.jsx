3ae7d50ad681bdfbd5e56d35f3be0b8b9c788ca5
Ketan <ketanm161@gmail.com>
2024-01-04 23:13:52 +0530

    added pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import About from "./Pages/About";
+import Profile from "./Pages/Profile";
 
 export default function App() {
 	return (
@@ -9,6 +14,7 @@ export default function App() {
 				<Route path="/sign-in" element={<SignIn />} />
 				<Route path="/sing-up" element={<SignUp />} />
 				<Route path="/about" element={<About />} />
+				<Route path="/profile" element={<Profile />} />
 			</Routes>
 		</BrowserRouter>
 	);
