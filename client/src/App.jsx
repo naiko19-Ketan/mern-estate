e7c0fd3c0f4c3f822e14e037d973b0fe12a8d9ae
Ketan <156310320+19ketan09@users.noreply.github.com>
2024-04-21 11:54:37 +0530

    feat: minor changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,10 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import SignIn from "./Pages/SignIn";
-import SignUp from "./Pages/SignUp";
 import About from "./Pages/About";
 import Profile from "./Pages/Profile";
 import Header from "./components/Header";
+import SignOut from "./Pages/SignOut";
 
 export default function App() {
 	return (
@@ -14,7 +13,7 @@ export default function App() {
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/sign-in" element={<SignIn />} />
-				<Route path="/sing-up" element={<SignUp />} />
+				<Route path="/sing-out" element={<SignOut />} />
 				<Route path="/about" element={<About />} />
 				<Route path="/profile" element={<Profile />} />
 			</Routes>
